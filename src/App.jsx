568539f530fd4352c0fc99ff066f5581e92295c4
Tushar <tusharkumarsahu1998@gmail.com>
2024-03-07 19:46:21 +0530

    make some changes for deployement

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-import React, { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import StarredImagesPage from "./pages/StarredImagesPage/StarredImagesPage";
 
 const App = () => {
-  const [isHomePageVisible, setIsHomePageVisible] = useState(true);
-
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          isHomePageVisible ? (
-            <HomePage />
-          ) : (
-            <Navigate to="/starredimages" replace />
-          )
-        }
-      />
-      <Route path="/starredimages" element={<StarredImagesPage />} />
+      <Route path="/" exact element={<HomePage />} />
+      <Route path="/starredimages" exact element={<StarredImagesPage />} />
     </Routes>
   );
 };
